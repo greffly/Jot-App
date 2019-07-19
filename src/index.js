5c765c5971fdc21d5e38e64b460f190d7985a2a6
Caitlyn Greffly <cgreffly@gmail.com>
2019-07-19 16:29:50 -0700

    add history

--- src/index.js
+++ src/index.js
@@ -11,8 +11,13 @@ import App from './App/App'
 
 library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
 
+export default function Routes() {
+  const history = createHistory({
+    basename: process.env.PUBLIC_URL,
+  });
+
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
